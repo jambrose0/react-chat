8689d6752af4fecc09c4d0b6bae1b7834b38800e
jambrose0 <jambrose0518@gmail.com>
2023-01-30 12:19:04 -0500

    users can sign in with email and password, even upload pictures. Moving onto creatingmultiple  db w/ firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// require('dotenv').config()
-
+import { getStorage } from "firebase/storage";
+import { getFirestore } from 'firebase/firestore'
 
 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.Chat_API_Key,
+  apiKey: 'AIzaSyCtq95n3qWgHtXEVIXhgTufXQzdefNkwMA',
   authDomain: "chatv3-69022.firebaseapp.com",
   projectId: "chatv3-69022",
   storageBucket: "chatv3-69022.appspot.com",
@@ -15,6 +15,9 @@ const firebaseConfig = {
   appId: "1:51470359230:web:11167e419b1f58942bdcef"
 };
 
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
+export const storage = getStorage();
+export const db = getFirestore();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { useState} from 'react'
 import add from '../img/add.png'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { auth,storage, db } from '../firebase'
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore"; 
 // import { useNavigate } from 'react-router-dom'
-import {  createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../firebase'
-
 
 export const  Register=() => {
+const [err, setErr] = useState(false)
 
-const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
@@ -15,20 +18,43 @@ const handleSubmit = (e) => {
     const file = e.target[3].files[0];
 
     console.log(displayName, email, password)
+try{
+    //creates users
+   const res= await createUserWithEmailAndPassword(auth, email, password);
 
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
+   //create references so users can upload images
+  const storageRef = ref(storage, displayName);
+   
+   const uploadTask = uploadBytesResumable(storageRef, file);
+   
 
-    console.log(user);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+   uploadTask.on((error) => {
+    setErr(true)
+     }, 
+     (error) => {
+     }, 
+     () => {
+       getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+         await updateProfile(res.user, {
+            displayName,
+            photoURL: downloadURL,
+         })
+         await setDoc(doc(db, "users", res.user.uid), {
+            uid: res.user.uid,
+            displayName,
+            email,
+            photoURL: downloadURL,
+          }          );
+
+          await setDoc(doc(db, "userChats", res.user.uid), {})
+       });
+     }
+   );
+
+
+} catch (err) {
+    setErr(true);
+}
 
 
 }
@@ -49,8 +75,8 @@ createUserWithEmailAndPassword(auth, email, password)
          <button>Sign Up</button>
         
          {/* {err && <span>Something went wrong</span>} */}
-        
-         <p>Do you already have an account? Login</p>
+         {err && <span>Something went wrong</span>}
+     <p>Do you already have an account? Login</p>
         </form>
     
             </div>
